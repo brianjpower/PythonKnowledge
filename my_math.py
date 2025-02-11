301a3e0f2320d29352abf93fd3f8664392063c21
Brian Power <brian.j.power@gmail.com>
2025-02-11 17:09:19 +0000

    Added mny_math module and called it from main

---
+++ my_math.py
@@ -0,0 +1,17 @@
+def odd_numbers(n):
+    i = 0
+    while i < n:
+        i += 1
+        if(i%2 != 0):
+            print(i)
+        else:
+            continue
+
+def even_numbers(n):
+    i = 0
+    while i < n:
+        i += 1
+        if(i%2 == 0):
+            print(i)
+        else:
+            continue
